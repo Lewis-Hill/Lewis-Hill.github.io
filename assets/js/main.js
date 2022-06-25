a64f1aceca9951f889913d8d3e9d111df3d640ed
Lewis-Hill <lewishill81@gmail.com>
2022-06-25 21:28:02 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,35 +36,6 @@ function showSlides(n) {
 
 (function($) {
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// to all images -- note I'm using a class!
-var images = document.getElementsByClassName('myImages');
-// the image in the modal
-var modalImg = document.getElementById("img01");
-// and the caption in the modal
-var captionText = document.getElementById("caption");
-
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-	var img = images[i];
-	// and attach our click listener for this image.
-	img.onclick = function(evt) {
-	  console.log(evt);
-	  modal.style.display = "block";
-	  modalImg.src = this.src;
-	  captionText.innerHTML = this.alt;
-	}
-  }
-
-  // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closed")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-  
 	var	$window = $(window),
 		$body = $('body'),
 		$header = $('#header'),
@@ -200,5 +171,34 @@ span.onclick = function() {
 						$menu._hide();
 
 			});
+
+			// Get the modal
+var modal = document.getElementById("myModal");
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+	var img = images[i];
+	// and attach our click listener for this image.
+	img.onclick = function(evt) {
+	  console.log(evt);
+	  modal.style.display = "block";
+	  modalImg.src = this.src;
+	  captionText.innerHTML = this.alt;
+	}
+  }
+
+  // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closed")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 })(jQuery);
