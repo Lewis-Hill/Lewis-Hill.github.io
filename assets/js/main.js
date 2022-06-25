97fcb37ec2058d00d8096a2b7a6e817d6a64a081
Lewis-Hill <lewishill81@gmail.com>
2022-06-25 19:13:18 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("imagetest");
+var img = document.getElementById("imagetest");
+var modalImg = document.getElementById("images/Screenshots/Functionality/AddToInventory.png");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
