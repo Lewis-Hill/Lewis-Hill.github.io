2586601305968127c1e333e5bfaa1a5f6ffab52a
Lewis-Hill <lewishill81@gmail.com>
2022-06-25 21:42:30 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,35 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 
 (function($) {
 
@@ -173,32 +144,61 @@ function showSlides(n) {
 			});
 
 			// Get the modal
-var modal = document.getElementById("myModal");
-// to all images -- note I'm using a class!
-var images = document.getElementsByClassName('myImages');
-// the image in the modal
-var modalImg = document.getElementById("img01");
-// and the caption in the modal
-var captionText = document.getElementById("caption");
+			var modal = document.getElementById("myModal");
+			// to all images -- note I'm using a class!
+			var images = document.getElementsByClassName('myImages');
+			// the image in the modal
+			var modalImg = document.getElementById("img01");
+			// and the caption in the modal
+			var captionText = document.getElementById("caption");
 
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-	var img = images[i];
-	// and attach our click listener for this image.
-	img.onclick = function(evt) {
-	  console.log(evt);
-	  modal.style.display = "block";
-	  modalImg.src = this.src;
-	  captionText.innerHTML = this.alt;
-	}
-  }
+			// Go through all of the images with our custom class
+			for (var i = 0; i < images.length; i++) {
+				var img = images[i];
+				// and attach our click listener for this image.
+				img.onclick = function(evt) {
+				console.log(evt);
+				modal.style.display = "block";
+				modalImg.src = this.src;
+				captionText.innerHTML = this.alt;
+				}
+			}
 
-  // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closed")[0];
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("closed")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+			modal.style.display = "none";
+			}
 
 })(jQuery);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
