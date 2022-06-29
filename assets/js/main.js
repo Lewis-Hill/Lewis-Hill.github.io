021c2244f5158d50c97f579d8d091f3e030e41c5
Lewis-Hill <lewishill81@gmail.com>
2022-06-29 20:28:54 +0100

    see code

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,6 +172,22 @@
 			modal.style.display = "none";
 			}
 
+			var coll = document.getElementsByClassName("collapsible");
+			
+			var i;
+
+			for (i = 0; i < coll.length; i++) {
+			coll[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var content = this.nextElementSibling;
+				if (content.style.display === "block") {
+				content.style.display = "none";
+				} else {
+				content.style.display = "block";
+				}
+			});
+			}
+
 })(jQuery);
 
 let slideIndex = 1;
